d1e36314580f223e1d964a47d9a64d6c679da8f3
imenesesnrg <ingrid.meneses@nrg.com>
2026-03-23 00:52:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if ingredients_list:
 
 #new section to display smoothiefroot nutrition infromation
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
